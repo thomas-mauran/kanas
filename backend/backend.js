c6cb36aea59c8eb686aa2e1ab058119cce518bca
Thomas Mauran <thomas.mauran@etu.umontpellier.fr>
2025-01-21 16:30:00 +0100

    feat: add kanas

--- backend/backend.js
+++ backend/backend.js
@@ -13,16 +13,129 @@ app.use(cors());
 
 // Kana data: Hiragana and Katakana
 const kanaData = [
+    // A-row
     { kana: 'あ', romaji: 'a' },
     { kana: 'い', romaji: 'i' },
     { kana: 'う', romaji: 'u' },
     { kana: 'え', romaji: 'e' },
     { kana: 'お', romaji: 'o' },
+
+    // K-row
     { kana: 'か', romaji: 'ka' },
     { kana: 'き', romaji: 'ki' },
     { kana: 'く', romaji: 'ku' },
     { kana: 'け', romaji: 'ke' },
     { kana: 'こ', romaji: 'ko' },
+
+    // S-row
+    { kana: 'さ', romaji: 'sa' },
+    { kana: 'し', romaji: 'shi' },
+    { kana: 'す', romaji: 'su' },
+    { kana: 'せ', romaji: 'se' },
+    { kana: 'そ', romaji: 'so' },
+
+    // T-row
+    { kana: 'た', romaji: 'ta' },
+    { kana: 'ち', romaji: 'chi' },
+    { kana: 'つ', romaji: 'tsu' },
+    { kana: 'て', romaji: 'te' },
+    { kana: 'と', romaji: 'to' },
+
+    // N-row
+    { kana: 'な', romaji: 'na' },
+    { kana: 'に', romaji: 'ni' },
+    { kana: 'ぬ', romaji: 'nu' },
+    { kana: 'ね', romaji: 'ne' },
+    { kana: 'の', romaji: 'no' },
+
+    // H-row
+    { kana: 'は', romaji: 'ha' },
+    { kana: 'ひ', romaji: 'hi' },
+    { kana: 'ふ', romaji: 'fu' },
+    { kana: 'へ', romaji: 'he' },
+    { kana: 'ほ', romaji: 'ho' },
+
+    // M-row
+    { kana: 'ま', romaji: 'ma' },
+    { kana: 'み', romaji: 'mi' },
+    { kana: 'む', romaji: 'mu' },
+    { kana: 'め', romaji: 'me' },
+    { kana: 'も', romaji: 'mo' },
+
+    // Y-row
+    { kana: 'や', romaji: 'ya' },
+    { kana: 'ゆ', romaji: 'yu' },
+    { kana: 'よ', romaji: 'yo' },
+
+    // R-row
+    { kana: 'ら', romaji: 'ra' },
+    { kana: 'り', romaji: 'ri' },
+    { kana: 'る', romaji: 'ru' },
+    { kana: 'れ', romaji: 're' },
+    { kana: 'ろ', romaji: 'ro' },
+
+    // W-row
+    { kana: 'わ', romaji: 'wa' },
+    { kana: 'を', romaji: 'wo' },
+
+    // N
+    { kana: 'ん', romaji: 'n' },
+
+    // Dakuten (voiced sounds)
+    // G-row
+    { kana: 'が', romaji: 'ga' },
+    { kana: 'ぎ', romaji: 'gi' },
+    { kana: 'ぐ', romaji: 'gu' },
+    { kana: 'げ', romaji: 'ge' },
+    { kana: 'ご', romaji: 'go' },
+
+    // Z-row
+    { kana: 'ざ', romaji: 'za' },
+    { kana: 'じ', romaji: 'ji' },
+    { kana: 'ず', romaji: 'zu' },
+    { kana: 'ぜ', romaji: 'ze' },
+    { kana: 'ぞ', romaji: 'zo' },
+
+    // D-row
+    { kana: 'だ', romaji: 'da' },
+    { kana: 'ぢ', romaji: 'ji' },
+    { kana: 'づ', romaji: 'zu' },
+    { kana: 'で', romaji: 'de' },
+    { kana: 'ど', romaji: 'do' },
+
+    // B-row
+    { kana: 'ば', romaji: 'ba' },
+    { kana: 'び', romaji: 'bi' },
+    { kana: 'ぶ', romaji: 'bu' },
+    { kana: 'べ', romaji: 'be' },
+    { kana: 'ぼ', romaji: 'bo' },
+
+    // P-row
+    { kana: 'ぱ', romaji: 'pa' },
+    { kana: 'ぴ', romaji: 'pi' },
+    { kana: 'ぷ', romaji: 'pu' },
+    { kana: 'ぺ', romaji: 'pe' },
+    { kana: 'ぽ', romaji: 'po' },
+
+    // Handakuten (p-sounds)
+    // P-row (Handakuten)
+    { kana: 'ぱ', romaji: 'pa' },
+    { kana: 'ぴ', romaji: 'pi' },
+    { kana: 'ぷ', romaji: 'pu' },
+    { kana: 'ぺ', romaji: 'pe' },
+    { kana: 'ぽ', romaji: 'po' },
+
+    // Small Kana
+    { kana: 'ぁ', romaji: 'a' },
+    { kana: 'ぃ', romaji: 'i' },
+    { kana: 'ぅ', romaji: 'u' },
+    { kana: 'ぇ', romaji: 'e' },
+    { kana: 'ぉ', romaji: 'o' },
+    { kana: 'っ', romaji: 'tsu' },
+    { kana: 'ゃ', romaji: 'ya' },
+    { kana: 'ゅ', romaji: 'yu' },
+    { kana: 'ょ', romaji: 'yo' },
+    { kana: 'ゎ', romaji: 'wa' }
 ];
 
 // Endpoint to get a random kana
